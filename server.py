dfd7127115def7782b776562cfa4b9def9604e93
Kush Patel <111901629+kushpatelj86@users.noreply.github.com>
2024-01-30 07:14:42 -0800

    Update server.py

--- server.py
+++ server.py
@@ -6,31 +6,37 @@ from Cryptodome.Cipher import AES
 
 
 
-PORT_NUMBER = int(argv[1]) #arg 2 is port numbers
-DECRYPTION_KEY = argv[2] #arg 3 is decryption  key
+PORT_NUMBER = int(argv[1]) #arg 1 is port numbers
+DECRYPTION_KEY = argv[2].encode() #arg 2 is decryption  key
 
 serverSock = socket(AF_INET, SOCK_STREAM) 
 serverSock.bind(('', PORT_NUMBER)) 
 serverSock.listen(100)
 
 
-
-
 while True:
-
 	print("Waiting for clients to connect...")
 	cliSock, cliInfo = serverSock.accept()
 	print("Client connected from: " + str(cliInfo))
 	cliMsg = cliSock.recv(16)	
-	print("Client sent " + str(cliMsg.decode()))
+	print("Client sent " + str(cliMsg))
 	#encCipher = AES.new(key, AES.MODE_ECB)
 	#cipherText = encCipher.encrypt('h4jlf123fj676s0d1114311150114s11f2')
 	#print("Cipher text: ", key)
 	decCipher = AES.new(DECRYPTION_KEY, AES.MODE_ECB)
 	plainText = decCipher.decrypt(cliMsg)
 	#unpaddedDecryptedText = unpad(plainText,16)
-	print("Decrypted text: ", plainText)
+	print("Decrypted text: ", plainText.decode())
 	cliSock.close()
+
+
+
+
+
+
+
+
+
 
 
 
